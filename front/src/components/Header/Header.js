3ac4233680d4c590ab099b75d5cca0c41a5edc0f
Your Name <you@example.com>
2021-05-14 11:09:14 -0300

    Retira imports não utilizados do Header.

--- front/src/components/Header/Header.js
+++ front/src/components/Header/Header.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import Person from './assets/person.svg'
-import Styles from './header.module.css'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
-import Form from 'react-bootstrap/Form'
-import FormControl from 'react-bootstrap/FormControl'
-import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Styles from './header.module.css'
+import Person from './assets/person.svg'
 
 function Header({projectPath}) {
     return (
