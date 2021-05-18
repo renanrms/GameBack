1965b395ee90bd9c0bd5fd394a231195264b6cf4
João Pinheiro <jpinheirolp@gmail.com>
2021-05-17 23:07:51 -0300

    juntamos a pasta plugin em um só arquivo e colocamos na mesma pasta do jogo
    Co-authored-by: Renan Passos <renanrms@users.noreply.github.com>

---
+++ examples/flappySquare/gameback.js
@@ -0,0 +1,100 @@
+
+//get
+const API_BASE_URL = 'http://gameback.com';
+
+async function get(resource, token) {
+  const url = new URL(`${API_BASE_URL}${resource}`);
+
+  headers = {
+    "X-Auth-Token": token
+  }
+        
+  const response = await fetch(url.toString(), {
+    headers,
+    method: 'GET',
+  });
+
+  const data = await response.json();
+  if (!response.ok) {
+    throw data;
+  }
+
+  return data;
+}
+
+//post
+
+async function post(content, resource, token=null) {
+  const url = new URL(`${API_BASE_URL}${resource}`);
+
+  if (token == null) {
+    headers = { "Content-Type": "application/json" }
+  }
+  else {
+    headers = {
+      "Content-Type": "application/json",
+      "X-Auth-Token": token
+    }
+  }
+
+  const response = await fetch(url.toString(), {
+    headers,
+    method: 'POST',
+    body: JSON.stringify(content),
+  });
+
+  let data;
+  try {
+    data = await response.json();
+  } catch (e) {
+    data = null;
+  }
+
+  if (!response.ok) {
+    throw data;
+  } else {
+    return data;
+  }
+}
+
+// Função para criar conta.
+function register(username, password, state) {
+  post({username, password, state}, '/player/register')
+    .then(data => {
+      return data; // Melhor analisar o conteúdo de data para retornar a parte necessária!
+    });
+}
+
+// Classe para autenticação e todas as operações do player.
+class Player {
+  constructor(username, password) {
+    this.state = {
+      token: null,
+      authenticated: false
+    }
+    this.username = null;
+
+    post({username, password}, '/player/login')
+      .then(data => {
+        this.state.token = data["X-Auth-Token"];
+        this.state.authenticated = true;
+        this.username = username;
+      })
+  }
+
+  getState() {
+    data = get('/player/getState', this.token);
+    return data["state"];
+  }
+
+  getPlayer() {
+    data = get('/player/getPlayer', this.token);
+    return data;
+  }
+
+  executeEvent(route, data) {
+    post({route, data}, '/events', this.state.token);
+  }
+  
+}
+
